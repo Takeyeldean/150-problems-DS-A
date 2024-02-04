5f20422d4fe5d44a8c573e4b1f2c998c1b3eb3f5
Takeyeldean <109932636+Takeyeldean@users.noreply.github.com>
2024-02-04 12:04:10 +0200

    Update code.cpp

--- problems/Remove Duplicates from Sorted Array/code.cpp
+++ problems/Remove Duplicates from Sorted Array/code.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
+// Time complexity -> O(N)
     int removeDuplicates(vector<int>& nums) {
      int p =0;
      for(int i=1;i<nums.size();i++)
      {
-         if(nums[i] != nums[p]) // O(N)
+         if(nums[i] != nums[p])
          {
              nums[++p] = nums[i];
          }
