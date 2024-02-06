99d862f0ed616c18b91997ca91cb5fc04855a9d9
Takey Eldean <tekoo146@gmail.com>
2024-02-06 10:15:36 +0200

    Update code.cpp

--- problems/ Best Time to Buy and Sell Stock/code.cpp
+++ problems/ Best Time to Buy and Sell Stock/code.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mx = 0, mn = prices[0]; 
+        for (int i = 0; i < prices.size() - 1; i++) { // O(N)
+            mn = min(mn, prices[i]);
+            mx = max(mx, prices[i + 1] - mn);
+        }
+        return mx;
+    }
+};
